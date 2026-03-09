202108659a02327792a8b9c5f624009b3e0194a0
amartinez.byob@gmail.com <tu@email.com>
2026-03-09 12:54:54 -0300

    CAMBIO DE HORARIO Y PERSONAL

--- src/data/edificios.ts
+++ src/data/edificios.ts
@@ -3,19 +3,19 @@ import type { BuildingAssignment } from "./types";
 export const EDIFICIOS: BuildingAssignment[] = [
   {
     turnoId: 1,
-    asignacion: { lunes: "C2", jueves: "C6", viernes: "C1" },
+    asignacion: { lunes: "C2", jueves: "C1", viernes: "C5" },
   },
   {
     turnoId: 2,
-    asignacion: { martes: "C2", miercoles: "C6", jueves: "C1", viernes: "C5" },
+    asignacion: { martes: "C2", miercoles: "C5", jueves: "CA", viernes: "C1" },
   },
   {
     turnoId: 3,
     asignacion: {
       lunes: "C7",
       martes: "C1",
-      miercoles: "C4–C9",
-      jueves: "C5",
+      miercoles: "C4-7-9",
+      jueves: "C2",
       viernes: "C6",
     },
   },
@@ -31,14 +31,14 @@ export const EDIFICIOS: BuildingAssignment[] = [
   },
   {
     turnoId: 5,
-    asignacion: { lunes: "C1", martes: "C5", miercoles: "C2" },
+    asignacion: { lunes: "C1", martes: "C5", miercoles: "C6" },
   },
   {
     turnoId: 6,
     asignacion: {
-      martes: "C4–C9",
-      miercoles: "C5",
-      jueves: "C7",
+      martes: "CA",
+      miercoles: "C4-9",
+      jueves: "C6",
       viernes: "C2",
     },
   },
@@ -47,19 +47,19 @@ export const EDIFICIOS: BuildingAssignment[] = [
     asignacion: {
       lunes: "C5",
       martes: "C6",
-      miercoles: "C7",
-      jueves: "C4–C9",
-      viernes: "C7",
+      miercoles: "C1",
+      jueves: "C4-7-9",
+      viernes: "CA",
     },
   },
   {
     turnoId: 8,
     asignacion: {
       lunes: "C6",
-      martes: "Apoyo",
-      miercoles: "C1",
-      jueves: "C2",
-      viernes: "C4–C9",
+      martes: "CA",
+      miercoles: "CA",
+      jueves: "C5",
+      viernes: "C4-7-9",
     },
   },
   {
@@ -67,7 +67,7 @@ export const EDIFICIOS: BuildingAssignment[] = [
     asignacion: {
       lunes: "C4–C9",
       martes: "C7",
-      miercoles: "Apoyo",
+      miercoles: "CA",
       jueves: "Apoyo",
       viernes: "Apoyo",
     },
